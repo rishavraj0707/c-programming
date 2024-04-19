b9524f3a3d5e528c7e473ae845e2cef5786412a5
Rishav Raj <162738116+rishavraj0707@users.noreply.github.com>
2024-04-19 10:57:59 +0530

    added in prime or not

--- 21primeorNot.c
+++ 21primeorNot.c
@@ -1,21 +1,26 @@
 // Number is prime and not 
 
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,i,count=0;
-    printf("Enter the number=");
-    scanf("%d",&n);
-    for(i=1;i<=n/2;i++)
+int n, i, count = 0;
+
+printf("Enter a positive integer: ");
+scanf("%d",&n);
+
+for(i=2; i<=n/2; ++i)
+{
+    if(n%i==0)
     {
-        if(n%i==0)
-            count++;
-        if(count=2)
-            break;
+        count=1;
+        break;
     }
-    if(count=1)
-        printf("Prime number!");
-    else
-    printf("not prime!");
-    return 0;
+}
+
+if (count==0)
+    printf("%d is a prime number.",n);
+else
+    printf("%d is not a prime number.",n);
+
+return 0;
 }
