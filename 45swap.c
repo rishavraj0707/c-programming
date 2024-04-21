e1ab829bc17f1221b07330d4f53dd72c10ff7c8c
Rishav Raj <162738116+rishavraj0707@users.noreply.github.com>
2024-04-21 19:18:53 +0530

    added on 21Apr

---
+++ 45swap.c
@@ -0,0 +1,18 @@
+// swapping of two number using pointer
+
+#include<stdio.h>
+void swapr(int *, int *);
+int main()
+{
+    int a=10,b=20;
+    swapr (&a,&b);
+    printf("a=%d , b=%d\n",a,b);
+    return 0;
+}
+void swapr(int *x, int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
