8ee5b262fc672992a980569ff7dfe75ce4a7cd10
Rishav Raj <162738116+rishavraj0707@users.noreply.github.com>
2024-04-22 22:54:59 +0530

    added on 22Apr

---
+++ 53fibonacci.c
@@ -0,0 +1,21 @@
+// first 25 terms of a fibonacci series 
+
+#include<stdio.h>
+int fibo(int );
+int main()
+{
+    int terms=25,i,n=0;
+    for(i=0;i<terms;i++)
+    {
+        printf("%d\t",fibo(n));
+        n++;
+    }
+    return 0;
+}
+int fibo(int n)
+{
+    if(n==0||n==1)
+        return n;
+    else
+        return (fibo(n-1)+fibo(n-2));
+}
