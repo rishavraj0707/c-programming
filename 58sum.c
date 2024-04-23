74352a3d28993c29c03933394559bc0919d44523
Rishav Raj <162738116+rishavraj0707@users.noreply.github.com>
2024-04-23 17:24:43 +0530

    added on 23 Apr

--- 58sum.c
+++ 58sum.c
@@ -1,23 +1,17 @@
-// sum of 1-d array
+// sum of one dimensional array    ||  a={1,2,3,4,5} = 15  ||
 
 #include<stdio.h>
-int main()
-{
-    int a[5],b[5],c[5];
-    int i,j,k;
-    printf("Enter the element in the array a:");
-    for(i=0;i<5;i++)
-        scanf("%d\n",&a[i]);
-    printf("Enter the element in the array b:");
-    for(j=0;j<5;j++)
-        scanf("%d\n",&a[j]);
-    if(i==j)
-    {
-        for(k=0;k<5;k++)
-            c[k]=a[k]+b[k];
-        printf("Element of the c is %d\n",c[k]);
-    }
-    else   
-        printf("Addition will not performed!");
+int main(){
+    int  arr[100],size,i,sum=0;
+    printf("Enter array size:\n");
+    scanf("%d",&size);
+    printf("Enter array element");
+    for(i=0;i<size;i++)
+        scanf("%d",&arr[i]);
+    for(i=0;i<size;i++)
+        sum=sum+arr[i];
+
+    printf("sum of the array =%d\n",sum);
+
     return 0;
 }
