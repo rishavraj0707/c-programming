d696b67f67685da1e67f2998763f60bed6ce9b8d
Rishav Raj <162738116+rishavraj0707@users.noreply.github.com>
2024-04-25 21:52:31 +0530

    Added on 25Apr

--- 68selectionSORT.c
+++ 68selectionSORT.c
@@ -1,29 +1,29 @@
-// // selection sort usig loop
+// selection sort usig loop
 
-// #include<stdio.h>
-// int main()
-// {
-//     int i,j,n,temp,a[20];
-//     printf("n=");
-//     scanf("%d",&n);
-//     printf("Enter the element to be sorted");
-//     for(i=0;i<n;i++)
-//         scanf("%d",&a[i]);
-//     for(i=0;i<n-1;i++)
-//     {
-//         for(j=i+1;j<n;j++)
-//         {
-//             if(a[j]<a[i])
-//             {
-//                 temp=a[i];
-//                 a[i]=a[j];
-//                 a[j]=temp;
-//             }
-//         }
-//     }
-//     printf("output:\n");
-//     for(i=0;i<n;i++)
-//         printf("%d\n",a[i]);
+#include<stdio.h>
+int main()
+{
+    int i,j,n,temp,a[20];
+    printf("n=");
+    scanf("%d",&n);
+    printf("Enter the element to be sorted");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("output:\n");
+    for(i=0;i<n;i++)
+        printf("%d\n",a[i]);
     
-//     return 0;
-// }
+    return 0;
+}
