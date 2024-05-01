3d8d32a1b0cb6fc78afc1a92e838391bf4239900
Rishav Raj <162738116+rishavraj0707@users.noreply.github.com>
2024-05-01 20:20:10 +0530

    Added on 1 may

---
+++ 79strlen.c
@@ -0,0 +1,14 @@
+// find the length of the string
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char arr[]="Bumblebee";
+    int len1,len2;
+    len1=strlen(arr);
+    len2=strlen("Humpty Dumpty");
+    printf("string=%s  length=%d\n",arr,len1);
+    printf("string=%s  length=%d\n","Humpty Dumpty",len2);
+    return 0;
+}
